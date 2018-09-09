35a8fd0bb029edbea0b9f7826c74e7138b6753ec
Ubuntu <ubuntu@ip-172-31-41-53.eu-central-1.compute.internal>
2018-09-09 18:14:58 +0000

    =ignoring config file now

--- config/config.js
+++
@@ -1,6 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  db: 'mongodb://localhost:27017/db_example_123',
-  db_dev: 'mongodb://localhost:27017/db_dev_123',
-};
